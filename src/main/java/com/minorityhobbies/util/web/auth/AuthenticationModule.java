43539a2ca6772f9972cdc0c4c2e74d1691ff1064
Chris Wraith <chris@indigneously.co.uk>
2014-04-11 16:17:36 +0100

    Fixing superfluous constructor-arg in AuthenticationModule

--- src/main/java/com/minorityhobbies/util/web/auth/AuthenticationModule.java
+++ src/main/java/com/minorityhobbies/util/web/auth/AuthenticationModule.java
@@ -3,7 +3,7 @@ package com.minorityhobbies.util.web.auth;
 public class AuthenticationModule {
 	private final AuthenticationService authenticationService;
 
-	public AuthenticationModule(AuthenticationService authenticationService) {
+	public AuthenticationModule() {
 		super();
 		this.authenticationService = new ScribeAuthenticationService();
 	}
